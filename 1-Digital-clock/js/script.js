560495b00633a4c7590f0a25ad69b97f6ebbf444
Mahir7878 <mahirmahir8951@gmail.com>
2024-03-16 18:24:38 +0530

    some-project

---
+++ 1-Digital-clock/js/script.js
@@ -0,0 +1,31 @@
+const hour = document.getElementById('hours')
+const minute = document.getElementById('minutes')
+const second = document.getElementById('second')
+const ampmEle = document.getElementById('am-pm')
+console.log(hour);
+console.log(ampmEle);
+
+function clock(){
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM"
+
+    if(h > 12){
+        h -= 12;
+        ampm = "PM";
+    }
+
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
+
+    hour.innerText = h;
+    minute.innerText = m;
+    second.innerText = s;
+    ampmEle.innerText = ampm;
+
+}
+setInterval(()=>{
+    clock()
+},1000)
