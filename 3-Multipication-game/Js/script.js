577d6d5ea33104f20dcabfa7b6ab7b626a2e72cc
Mahir7878 <mahirmahir8951@gmail.com>
2024-03-27 11:23:41 +0530

    update-code

--- 3-Multipication-game/Js/script.js
+++ 3-Multipication-game/Js/script.js
@@ -1,36 +1,21 @@
+const [form, question, scoreValue, inputField] = [
+    document.querySelector("form"),
+    document.getElementById("question"),
+    document.querySelector(".score-value"),
+    document.querySelector(".input")
+];
+let [score, answer] = [0, questionGenerate()];
 
-const form = document.querySelector(".form");
-const scoreval = document.querySelector(".score-value");
-const question = document.querySelector("#question");
-const input = document.querySelector(".input");
-
-let score = 0; 
-
-let ans = generateQuestion(); 
-
-function generateQuestion() {
-    const num1 = Math.floor(Math.random() * 10);
-    const num2 = Math.floor(Math.random() * 10);
-    question.innerHTML = `What is ${num1} Multiply by ${num2}`;
+function questionGenerate() {
+    const [num1, num2] = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+    question.textContent = `What's ${num1} multiplied by ${num2}?`
     return num1 * num2;
 }
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", e => {
     e.preventDefault();
-    
-    const userAns = parseInt(input.value); 
-    
-    if (userAns === ans) {
-        score++; 
-        scoreval.textContent = score;
-
-    } else {
-        score--; 
-        scoreval.textContent = score;
-    }
-    
-    ans = generateQuestion()
-    input.value = "";
+    score += +inputField.value === answer ? 1 : -1;
+    scoreValue.textContent = score;
+    answer = questionGenerate();
+    form.reset();
 });
-
-
