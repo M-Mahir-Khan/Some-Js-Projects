12cc26a4375c8b665e0902ee4ecdf79e823feafc
Mahir7878 <mahirmahir8951@gmail.com>
2024-09-29 00:24:38 +0530

    Number Quiz Game

--- Number Quiz Game/Js/script.js
+++ Number Quiz Game/Js/script.js
@@ -4,12 +4,18 @@ const [form, question, scoreValue, inputField] = [
     document.querySelector(".score-value"),
     document.querySelector(".input")
 ];
+
+let operators = ["+", "-", "*", "/"]
+let operatorName = ["plus", "minus", "multiply by", "divide by"]
 let [score, answer] = [0, questionGenerate()];
 
 function questionGenerate() {
     const [num1, num2] = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
-    question.textContent = `What's ${num1} multiplied by ${num2}?`
-    return num1 * num2;
+    let index = Math.floor(Math.random() * 3);
+
+    question.textContent = `What's ${num1} ${operatorName[index]} ${num2}?`
+    let res = eval(`${num1} ${operators[index]} ${num2}`)
+    return res
 }
 
 form.addEventListener("submit", e => {
